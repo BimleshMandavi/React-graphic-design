7fea0dfa5043590d0d9061df54c93040ad1307f0
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2025-01-05 19:49:51 +0530

    commit-13

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -12,108 +12,108 @@ export default function TakeAdvantageSection() {
       title: "Logo Design",
       description:
         "Craft unique and memorable logos that embody your brand’s identity, leaving a lasting impression on your audience.",
-      icon: "/assets/open-book.png",
+     icon: "/pic-13.png",
     },
     {
       title: "Brand Identity Design",
       description:
         "Developing complete branding elements, including color palettes, typography, and visual style guidelines.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Social Media Graphics",
       description:
         "Creating visually engaging graphics for social media posts, ads, stories, and banners.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Website and App Graphics",
       description:
         "Designing UI/UX visuals, icons, and elements for websites and mobile applications.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Infographics",
       description:
         "Converting complex data and information into visually appealing and easy-to-understand infographics.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Ad Banners",
       description:
         "Develop eye-catching ad banners optimized for digital campaigns to maximize clicks and conversions.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Motion Graphics and GIFs",
       description: ".....",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Brochure & Flyers",
       description:
         "Design informative and visually appealing brochures and flyers that effectively convey your message and promote your business.",
-      icon: "/assets/open-book.png",
+     icon: "/pic-13.png",
     },
     {
       title: "Illustration Services",
       description:
         "Producing custom illustrations tailored to enhance brand communication and storytelling.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Presentation Design",
       description:
         "Create compelling PowerPoint templates and pitch decks that captivate audiences and communicate your message effectively.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Advertising Design",
       description:
         "Designing print and digital ads, including billboards, banners, and magazine layouts, for impactful campaigns.",
-      icon: "/assets/open-book.png",
+     icon: "/pic-13.png",
     },
     {
       title: "Ebook and Publication",
       description:
         "Designing layouts for ebooks, magazines, and newsletters with consistent and appealing visual aesthetics.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Menu Design",
       description:
         "Crafting attractive and easy-to-navigate menus for restaurants, cafes, or events.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Poster and Banner Design",
       description:
         "Creating impactful posters and banners for promotions, events, or campaigns.",
-      icon: "/assets/open-book.png",
+     icon: "/pic-13.png",
     },
     {
       title: "Custom Typography Design",
       description:
         "Creating unique fonts and typography tailored to brand requirements.",
-      icon: "/assets/open-book.png",
+      iicon: "/pic-13.png",
     },
     {
       title: "Book Cover Design",
       description:
         "Crafting visually appealing and market-relevant book covers for print and digital publications.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "UI Icon Design",
       description:
         "Crafting consistent, functional, and aesthetically pleasing icons for apps and websites.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
     {
       title: "Brand Asset Creation",
       description:
         "Developing a library of cohesive design assets to ensure consistent branding across platforms.",
-      icon: "/assets/open-book.png",
+      icon: "/pic-13.png",
     },
   ];
 
@@ -127,7 +127,7 @@ export default function TakeAdvantageSection() {
     >
       <div className="container mx-auto">
         <motion.h2
-          className="text-3xl md:text-6xl  text-center text-black mb-12"
+          className="text-3xl md:text-6xl  text-start text-black mb-12"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.6, delay: 0.2 }}
@@ -138,7 +138,7 @@ export default function TakeAdvantageSection() {
           {items.map((item, index) => (
             <motion.div
               key={index}
-              className="bg-white shadow-lg p-6 rounded-lg flex flex-col items-center text-center"
+              className="bg-white shadow-lg p-8 rounded-lg flex flex-col  "
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.6, delay: 0.3 + index * 0.1 }}
@@ -149,11 +149,11 @@ export default function TakeAdvantageSection() {
                 src={item.icon}
                 alt={item.title}
             
-                className="w-12 h-12 mb-4"
+                className="w-10 h-10 mb-4"
               />
-              <h3 className="text-lg text-gray-600 font-bold mb-2">{item.title}</h3>
+              <h3 className="text-2xl text-black  mb-2">{item.title}</h3>
               </div>
-              <p className="text-sm text-gray-600">{item.description}</p>
+              <p className="text-lg text-gray-600">{item.description}</p>
             </motion.div>
           ))}
         </div>
